0c2ad0528f4897dc7d22059fd5868a81c84bf5e0
oatsu <panchi.psp@gmail.com>
2021-07-11 02:17:30 +0900

    ステージ7を追加し、release準備をすることにした。

--- train/prepare_model_for_release_enunu.py
+++ train/prepare_model_for_release_enunu.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 # Copyright (c) 2021 oatsu
 """
 配布用フォルダを準備する
@@ -6,20 +6,12 @@
 
 from glob import glob
 from os import makedirs
-from os.path import basename
+from os.path import exists
 from shutil import copy2, copytree
 
 import yaml
+from send2trash import send2trash
 from tqdm import tqdm
-
-with open('config.yaml', 'r') as f_yaml:
-    config = yaml.load(f_yaml, Loader=yaml.FullLoader)
-SINGER = config['spk'].strip('"\'')
-CONFIG_DIR = 'conf'
-PATH_TABLE = config['table_path'].strip('"\'')
-RELEASE_DIR = f'release/{SINGER}_---'
-PATH_QUESTION = config['question_path'].strip('"\'')
-NAME_EXPERIMENT = config['tag'].strip('"\'')
 
 
 def copy_train_config(config_dir, release_dir):
@@ -75,6 +67,7 @@ def copy_model(singer, name_exp, release_dir):
     for path_model in tqdm(list_path_model):
         copy2(path_model, f'{release_dir}/{path_model}')
 
+
 def copy_general_config(release_dir):
     """
     singer: 歌唱者名
@@ -95,14 +88,28 @@ def main():
     """
     各種ファイルをコピーする
     """
-    makedirs(RELEASE_DIR, exist_ok=True)
-    copy_general_config(RELEASE_DIR)
-    copy_enuconfig(RELEASE_DIR)
-    # copy_train_config(CONFIG_DIR, RELEASE_DIR)
-    copy_dictionary(PATH_TABLE, RELEASE_DIR)
-    copy_question(PATH_QUESTION, RELEASE_DIR)
-    copy_scaler(SINGER, RELEASE_DIR)
-    copy_model(SINGER, NAME_EXPERIMENT, RELEASE_DIR)
+    # load settings
+    with open('config.yaml', 'r') as f_yaml:
+        config = yaml.load(f_yaml, Loader=yaml.FullLoader)
+    singer = config['spk'].strip('"\'')
+    config_dir = 'conf'
+    release_dir = f'release/{singer}_---'
+    path_table = config['table_path'].strip('"\'')
+    path_question = config['question_path'].strip('"\'')
+    experiment_name = config['tag'].strip('"\'')
+
+    # copy models to the release directory
+    if exists(release_dir):
+        print('Sending existing directory to recycle bin')
+        send2trash(release_dir)
+    makedirs(release_dir, exist_ok=True)
+    copy_general_config(release_dir)
+    copy_enuconfig(release_dir)
+    copy_train_config(config_dir, release_dir)
+    copy_dictionary(path_table, release_dir)
+    copy_question(path_question, release_dir)
+    copy_scaler(singer, release_dir)
+    copy_model(singer, experiment_name, release_dir)
 
 
 if __name__ == '__main__':
