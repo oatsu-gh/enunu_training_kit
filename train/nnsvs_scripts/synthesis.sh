4962fd394453cc162db5e9db63436035d1edef85
oatsu <oatsu.dev@gmail.com>
2025-08-16 13:48:56 +0900

    dtype を指定できるようにしたい

--- train/nnsvs_scripts/synthesis.sh
+++ train/nnsvs_scripts/synthesis.sh
@@ -42,27 +42,28 @@ for s in ${testsets[@]}; do
         else
             ground_truth_duration=true
         fi
-
+        
         xrun $PYTHON_EXE -m nnsvs.bin.synthesis $ext \
-            synthesis=$synthesis \
-            synthesis.sample_rate=$sample_rate \
-            synthesis.qst=$question_path \
-            synthesis.ground_truth_duration=$ground_truth_duration \
-            timelag.checkpoint=$expdir/${timelag_model}/$timelag_eval_checkpoint \
-            timelag.in_scaler_path=$dump_norm_dir/in_timelag_scaler.joblib \
-            timelag.out_scaler_path=$dump_norm_dir/out_timelag_scaler.joblib \
-            timelag.model_yaml=$expdir/${timelag_model}/model.yaml \
-            duration.checkpoint=$expdir/${duration_model}/$duration_eval_checkpoint \
-            duration.in_scaler_path=$dump_norm_dir/in_duration_scaler.joblib \
-            duration.out_scaler_path=$dump_norm_dir/out_duration_scaler.joblib \
-            duration.model_yaml=$expdir/${duration_model}/model.yaml \
-            acoustic.checkpoint=$expdir/${acoustic_model}/$acoustic_eval_checkpoint \
-            acoustic.in_scaler_path=$dump_norm_dir/in_acoustic_scaler.joblib \
-            acoustic.out_scaler_path=$dump_norm_dir/out_acoustic_scaler.joblib \
-            acoustic.model_yaml=$expdir/${acoustic_model}/model.yaml \
-            vocoder.checkpoint=$vocoder_eval_checkpoint \
-            utt_list=./data/list/$s.list \
-            in_dir=data/acoustic/$input/ \
-            out_dir=$expdir/$dst_name/$s/$input
+        synthesis=$synthesis \
+        synthesis.sample_rate=$sample_rate \
+        synthesis.qst=$question_path \
+        synthesis.ground_truth_duration=$ground_truth_duration \
+        timelag.checkpoint=$expdir/${timelag_model}/$timelag_eval_checkpoint \
+        timelag.in_scaler_path=$dump_norm_dir/in_timelag_scaler.joblib \
+        timelag.out_scaler_path=$dump_norm_dir/out_timelag_scaler.joblib \
+        timelag.model_yaml=$expdir/${timelag_model}/model.yaml \
+        duration.checkpoint=$expdir/${duration_model}/$duration_eval_checkpoint \
+        duration.in_scaler_path=$dump_norm_dir/in_duration_scaler.joblib \
+        duration.out_scaler_path=$dump_norm_dir/out_duration_scaler.joblib \
+        duration.model_yaml=$expdir/${duration_model}/model.yaml \
+        acoustic.checkpoint=$expdir/${acoustic_model}/$acoustic_eval_checkpoint \
+        acoustic.in_scaler_path=$dump_norm_dir/in_acoustic_scaler.joblib \
+        acoustic.out_scaler_path=$dump_norm_dir/out_acoustic_scaler.joblib \
+        acoustic.model_yaml=$expdir/${acoustic_model}/model.yaml \
+        vocoder.checkpoint=$vocoder_eval_checkpoint \
+        utt_list=./data/list/$s.list \
+        in_dir=data/acoustic/$input/ \
+        out_dir=$expdir/$dst_name/$s/$input \
+        synthesis.dtype=$dtype
     done
 done
