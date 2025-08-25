5a133bf107446057080454fa6ecd2d57a9c4ee82
oatsu <oatsu.dev@gmail.com>
2025-08-26 00:04:32 +0900

    applied ruff

--- train/nnsvs_scripts/nnsvs2usfgan.py
+++ train/nnsvs_scripts/nnsvs2usfgan.py
@@ -1,5 +1,5 @@
-"""Convert NNSVS's pre-processed features to usfgan-friendly format
-"""
+"""Convert NNSVS's pre-processed features to usfgan-friendly format"""
+
 import argparse
 import os
 import sys
@@ -59,18 +59,14 @@ def get_parser():
     parser = argparse.ArgumentParser(
         description="Convert NNSVS's pre-processed features to usfgan's format",
     )
-    parser.add_argument("config_path", type=str, help="Path to recipe config")
-    parser.add_argument("out_dir", type=str, help="Output directory")
-    parser.add_argument(
-        "--feature_type", type=str, default="world", help="Feature type"
-    )
-    parser.add_argument(
-        "--relative_path", action="store_true", help="Use relative path"
-    )
+    parser.add_argument('config_path', type=str, help='Path to recipe config')
+    parser.add_argument('out_dir', type=str, help='Output directory')
+    parser.add_argument('--feature_type', type=str, default='world', help='Feature type')
+    parser.add_argument('--relative_path', action='store_true', help='Use relative path')
     return parser
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = get_parser().parse_args(sys.argv[1:])
     recipe_config_path = Path(args.config_path)
     assert recipe_config_path.exists()
@@ -81,102 +77,94 @@ if __name__ == "__main__":
     sample_rate = recipe_config.sample_rate
 
     # NOTE: used for de-normalization
-    scaler = joblib.load(f"dump/{spk}/norm/out_acoustic_scaler.joblib")
+    scaler = joblib.load(f'dump/{spk}/norm/out_acoustic_scaler.joblib')
 
     # Save scaler for usfgan
-    out_stats_dir = Path(f"{args.out_dir}/stats")
+    out_stats_dir = Path(f'{args.out_dir}/stats')
     out_stats_dir.mkdir(parents=True, exist_ok=True)
 
     # NOTE: mgc order is fixed regardless of sampling rate
-    if args.feature_type == "world":
+    if args.feature_type == 'world':
         if len(scaler.mean_) > 65:
             # (mgc, lf0, vuv, mcap)
             stream_sizes = [60, 1, 1, len(scaler.mean_) - 62]
             # NOTE: pretend codeap as mcap
-            feat_types = ["f0", "contf0", "mcep", "codeap"]
+            feat_types = ['f0', 'contf0', 'mcep', 'codeap']
         else:
             # (mgc, lf0, vuv, bap)
             stream_sizes = [60, 1, 1, pyworld.get_num_aperiodicities(sample_rate)]
-            feat_types = ["f0", "contf0", "mcep", "codeap"]
+            feat_types = ['f0', 'contf0', 'mcep', 'codeap']
         # NOTE: scaler for F0 is dummy and never used at usfgan training
         assert len(scaler.mean_.reshape(-1)) == sum(stream_sizes)
 
         usfgan_scaler = {
-            "mcep": StandardScaler(
-                scaler.mean_[0:60], scaler.var_[0:60], scaler.scale_[0:60]
-            ),
-            "f0": StandardScaler(
-                scaler.mean_[60:61], scaler.var_[60:61], scaler.scale_[60:61]
-            ),
-            "contf0": StandardScaler(
+            'mcep': StandardScaler(scaler.mean_[0:60], scaler.var_[0:60], scaler.scale_[0:60]),
+            'f0': StandardScaler(scaler.mean_[60:61], scaler.var_[60:61], scaler.scale_[60:61]),
+            'contf0': StandardScaler(
                 scaler.mean_[60:61], scaler.var_[60:61], scaler.scale_[60:61]
             ),
             # NOTE: cf0 is for sifigan repo. it is better to unify them in the future.
-            "cf0": StandardScaler(
-                scaler.mean_[60:61], scaler.var_[60:61], scaler.scale_[60:61]
-            ),
-            "codeap": StandardScaler(
-                scaler.mean_[62:], scaler.var_[62:], scaler.scale_[62:]
-            ),
+            'cf0': StandardScaler(scaler.mean_[60:61], scaler.var_[60:61], scaler.scale_[60:61]),
+            'codeap': StandardScaler(scaler.mean_[62:], scaler.var_[62:], scaler.scale_[62:]),
         }
-    elif args.feature_type == "melf0":
+    elif args.feature_type == 'melf0':
         stream_sizes = [len(scaler.mean_) - 2, 1, 1]
-        feat_types = ["f0", "contf0", "logmsp"]
+        feat_types = ['f0', 'contf0', 'logmsp']
         mel_dim = stream_sizes[0]
         usfgan_scaler = {
-            "logmsp": StandardScaler(
+            'logmsp': StandardScaler(
                 scaler.mean_[0:mel_dim],
                 scaler.var_[0:mel_dim],
                 scaler.scale_[0:mel_dim],
             ),
-            "f0": StandardScaler(
+            'f0': StandardScaler(
                 scaler.mean_[mel_dim : mel_dim + 1],
                 scaler.var_[mel_dim : mel_dim + 1],
                 scaler.scale_[mel_dim : mel_dim + 1],
             ),
-            "contf0": StandardScaler(
+            'contf0': StandardScaler(
                 scaler.mean_[mel_dim : mel_dim + 1],
                 scaler.var_[mel_dim : mel_dim + 1],
                 scaler.scale_[mel_dim : mel_dim + 1],
             ),
-            "cf0": StandardScaler(
+            'cf0': StandardScaler(
                 scaler.mean_[mel_dim : mel_dim + 1],
                 scaler.var_[mel_dim : mel_dim + 1],
                 scaler.scale_[mel_dim : mel_dim + 1],
             ),
         }
     else:
-        raise ValueError(f"Unknown feature type: {args.feature_type}")
+        raise ValueError(f'Unknown feature type: {args.feature_type}')
 
-    joblib.dump(usfgan_scaler, out_stats_dir / "scaler.joblib")
+    joblib.dump(usfgan_scaler, out_stats_dir / 'scaler.joblib')
 
     hop_size = -1
     aux_channels = -1
 
     out_dir = Path(args.out_dir)
     out_dir.mkdir(exist_ok=True, parents=True)
-    for s in ["dev", "eval", "train_no_dev"]:
-        list_file = Path(f"data/list/{s}.list")
+    for s in ['dev', 'eval', 'train_no_dev']:
+        list_file = Path(f'data/list/{s}.list')
         assert list_file.exists()
         utt_ids = load_utt_list(list_file)
-        dump_norm_dir = Path(f"dump/{spk}/norm/{s}/in_vocoder")
+        dump_norm_dir = Path(f'dump/{spk}/norm/{s}/in_vocoder')
 
         # Output directories
-        out_scp_dir = out_dir / "scp"
-        out_hdf5_dir = out_dir / "hdf5"
-        out_wav_dir = out_dir / "wav"
+        out_scp_dir = out_dir / 'scp'
+        out_hdf5_dir = out_dir / 'hdf5'
+        out_wav_dir = out_dir / 'wav'
         for d in [out_scp_dir, out_hdf5_dir, out_wav_dir]:
             d.mkdir(exist_ok=True, parents=True)
 
         for utt_id in tqdm(utt_ids, desc=s):
-            wave = np.load(dump_norm_dir / f"{utt_id}-wave.npy")
-            feats = np.load(dump_norm_dir / f"{utt_id}-feats.npy")
+            wave = np.load(dump_norm_dir / f'{utt_id}-wave.npy')
+            feats = np.load(dump_norm_dir / f'{utt_id}-feats.npy')
 
             feats = scaler.inverse_transform(feats)
-            if args.feature_type == "world":
+            if args.feature_type == 'world':
                 mgc, lf0, vuv, bap = split_streams(feats, stream_sizes)
                 aux_channels = mgc.shape[-1] + bap.shape[-1]
-            elif args.feature_type == "melf0":
+            elif args.feature_type == 'melf0':
                 mel, lf0, vuv = split_streams(feats, stream_sizes)
                 aux_channels = mel.shape[1]
 
@@ -194,49 +182,49 @@ if __name__ == "__main__":
             f0 = contf0.copy()
             f0[vuv < 0.5] = 0
 
-            usfgan_feat_path = out_hdf5_dir / f"{utt_id}.h5"
-            write_hdf5(usfgan_feat_path, "/uv", vuv)
-            write_hdf5(usfgan_feat_path, "/f0", f0)
-            write_hdf5(usfgan_feat_path, "/contf0", contf0)
-            write_hdf5(usfgan_feat_path, "/cf0", contf0)
+            usfgan_feat_path = out_hdf5_dir / f'{utt_id}.h5'
+            write_hdf5(usfgan_feat_path, '/uv', vuv)
+            write_hdf5(usfgan_feat_path, '/f0', f0)
+            write_hdf5(usfgan_feat_path, '/contf0', contf0)
+            write_hdf5(usfgan_feat_path, '/cf0', contf0)
 
-            if args.feature_type == "world":
-                write_hdf5(usfgan_feat_path, "/mcep", mgc)
-                write_hdf5(usfgan_feat_path, "/codeap", bap)
-            elif args.feature_type == "melf0":
-                write_hdf5(usfgan_feat_path, "/logmsp", mel)
+            if args.feature_type == 'world':
+                write_hdf5(usfgan_feat_path, '/mcep', mgc)
+                write_hdf5(usfgan_feat_path, '/codeap', bap)
+            elif args.feature_type == 'melf0':
+                write_hdf5(usfgan_feat_path, '/logmsp', mel)
             # NOTE: the following two features are not supported for now
             # write_hdf5(usfgan_feat_path, "/mcap", mcap)
 
             # NNSVSや林さんのparallel_waveganでは、波形は hdf5/npy フォーマットに前処理で変換
             # しているが、usfganは生データを読み込んでいるようなので、あわせる
             # NNSVSの前処理で波形を float32 に変換していますが、必要であれば、int16に変換してください
-            wavfile.write(out_wav_dir / f"{utt_id}.wav", sample_rate, wave.reshape(-1))
+            wavfile.write(out_wav_dir / f'{utt_id}.wav', sample_rate, wave.reshape(-1))
 
         # Write scp/list files for usfgan
         # NOTE: scp: 波形のパス, list: 特徴量のパス
-        with open(out_scp_dir / f"{spk}_sr{sample_rate}_{s}.scp", "w") as f:
+        with open(out_scp_dir / f'{spk}_sr{sample_rate}_{s}.scp', 'w') as f:
             for utt_id in utt_ids:
                 # usfganの実装を見る限り絶対パス前提になってるっぽいので、絶対パスにする
                 # 相対パスもサポートしてくれるといいかもしれない
-                wav_path = out_wav_dir / f"{utt_id}.wav"
+                wav_path = out_wav_dir / f'{utt_id}.wav'
                 assert wav_path.exists()
                 if args.relative_path:
-                    f.write(f"{wav_path}\n")
+                    f.write(f'{wav_path}\n')
                 else:
-                    f.write(f"{wav_path.resolve()}\n")
+                    f.write(f'{wav_path.resolve()}\n')
 
-        with open(out_scp_dir / f"{spk}_sr{sample_rate}_{s}.list", "w") as f:
+        with open(out_scp_dir / f'{spk}_sr{sample_rate}_{s}.list', 'w') as f:
             for utt_id in utt_ids:
-                feat_path = out_hdf5_dir / f"{utt_id}.h5"
+                feat_path = out_hdf5_dir / f'{utt_id}.h5'
                 assert feat_path.exists()
                 if args.relative_path:
-                    f.write(f"{feat_path}\n")
+                    f.write(f'{feat_path}\n')
                 else:
-                    f.write(f"{feat_path.resolve()}\n")
+                    f.write(f'{feat_path.resolve()}\n')
 
     # usfganの学習にあるとよい情報
-    if args.feature_type == "world":
+    if args.feature_type == 'world':
         print(
             f"""stream_sizes: {stream_sizes}
 hop_size: {hop_size}
@@ -245,7 +233,7 @@ aux_channels: {aux_channels}
 feat_types: {feat_types}
 aux_feats: ["mcep", "codeap"]"""
         )
-    elif args.feature_type == "melf0":
+    elif args.feature_type == 'melf0':
         print(
             f"""stream_sizes: {stream_sizes}
 hop_size: {hop_size}
