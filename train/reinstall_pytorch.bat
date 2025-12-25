9f0583c47ed26d4b5d89c118fbab72d8f3db758b
oatsu <oatsu.dev@gmail.com>
2025-12-25 23:41:52 +0900

    Delete reinstall_pytorch.bat

--- train/reinstall_pytorch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PYTHON_EXE=.\python-3.9.13-embed-amd64\python.exe
-
-%PYTHON_EXE% -m pip install --upgrade pip wheel setuptools --no-warn-script-location
-%PYTHON_EXE% -m pip install --upgrade light-the-torch --no-warn-script-location
-%PYTHON_EXE% -m pip uninstall -y torch torchaudio --no-warn-script-location
-%PYTHON_EXE% -m light-the-torch install --upgrade torch torchaudio
-
-pause
