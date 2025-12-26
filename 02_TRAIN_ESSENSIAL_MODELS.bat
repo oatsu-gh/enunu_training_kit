ca6e9176918e22a59097db822de0836801f92890
oatsu <oatsu.dev@gmail.com>
2025-12-26 20:39:23 +0900

    Update 02_TRAIN_ESSENSIAL_MODELS.bat

--- 02_TRAIN_ESSENSIAL_MODELS.bat
+++ 02_TRAIN_ESSENSIAL_MODELS.bat
@@ -2,5 +2,7 @@
 .\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 2 --stop_stage 3
 @REM train acoustic model
 .\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 4 --stop_stage 4
+@REM synthesis test
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 6 --stop_stage 6
 
 PAUSE
