85ae6bdeeca886a37dff44cdfd529d079a4e7139
oatsu <oatsu.dev@gmail.com>
2026-01-04 01:43:44 +0900

    Create 99_Train_All_Models.bat

---
+++ 99_Train_All_Models.bat
@@ -0,0 +1,26 @@
+@REM check path (tensorflow does not support japanese path)
+@powershell -NoProfile -Command "$path='%CD%'; if($path -match '[^\x00-\x7F]'){exit 1}else{exit 0}" >nul 2>&1
+@if %errorlevel% neq 0 (
+    echo === WARNING!! ================================================================================
+    echo  Current directory path has non-ASCII characters like Japanese Hiragana, Katakana, or Kanji.
+    echo  This will result errors. Rename the directory or run this in a different directory.
+    echo ==============================================================================================
+    exit /b 1
+)
+
+@REM train timelag and acoustic models
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 2 --stop_stage 3
+
+@REM train acoustic model
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 4 --stop_stage 4
+
+@REM synthesis test
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 6 --stop_stage 6
+
+@REM prepare vocoder training data (stage1 is required in advance)
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 9 --stop_stage 9
+
+@REM train vocoder (uSFGAN) model
+.\PortableGit-2.52.0\bin\bash.exe .\run.sh --stage 11 --stop_stage 11
+
+PAUSE
